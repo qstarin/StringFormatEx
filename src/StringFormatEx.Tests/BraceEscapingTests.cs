2b2daf61bc5990f78288090b8f56ede7378d86f3
qstarin <qstarin@gmail.com>
2010-10-12 13:10:38 -0500

    Fixed bug in closing brace escaping, added one test for it so far - brace escaping *will* be changing.

---
+++ src/StringFormatEx.Tests/BraceEscapingTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+
+
+namespace StringFormatEx.Tests
+{
+    [TestFixture]
+    public class BraceEscapingTests
+    {
+
+        [Test]
+        public void Escaping1()
+        {
+            var formatString = @" \{{0}\} \{1} \{\{{2:00\}00}-\} ";
+            var args = new object[] { "A", "B", 5555 };
+            var expectedOutput = " {A} {1} {{55}55-} ";
+
+
+            string actualOutput = ExtendedStringFormatter.Default.FormatEx(formatString, args);
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+
+    }
+}
